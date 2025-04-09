be6e88bee338b69157a8e71a5063da73b989ac81
Zixuan Cao <cao.zixuan@campus.lmu.de>
2025-04-10 03:52:49 +0800

    added a homepage

--- src/app.py
+++ src/app.py
@@ -8,11 +8,12 @@ def navigate_to(page_name):
 def main():
     st.set_page_config(page_title="Explanation Helper", page_icon=":guardsman:", layout="centered")
     # st.title("Explanation Helper")
+    home_page = st.Page(r"pages/p_home.py", title="Homepage")
     clss_eval_page = st.Page(r"pages/p_clss_eval.py", title="Classification Evaluation")
     expl_eval_page = st.Page(r"pages/p_expl_eval.py", title="Explanation Evaluation")
     expl_page = st.Page(r"pages/p_expl.py", title="Local Explanation")
     
-    pg = st.navigation([clss_eval_page, expl_eval_page, expl_page])
+    pg = st.navigation([home_page, clss_eval_page, expl_eval_page, expl_page])
     
     pg.run()
 
