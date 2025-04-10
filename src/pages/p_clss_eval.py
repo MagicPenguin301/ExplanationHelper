15e594fb638fba32eed1763f993485b2fc31277e
Zixuan Cao <cao.zixuan@campus.lmu.de>
2025-04-10 15:14:01 +0800

    UI for clss eval

--- src/pages/p_clss_eval.py
+++ src/pages/p_clss_eval.py
@@ -1,8 +1,13 @@
 import streamlit as st
+import utils
+import clss_eval
 
-
+@utils.after_model_loaded
 def run():
-    st.title("Classification Evaluation")
+    if test_file := st.file_uploader("Upload a test dataset."):
+        texts, trues = utils.read_data(test_file)
+        clss_eval.evaluate_and_visualize(texts, trues)
 
 if __name__ == "__main__":
+    st.title("Classification Evaluation")
     run()
